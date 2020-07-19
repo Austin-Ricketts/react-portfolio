752227153179602cd9d00cf2816da9d1a0df2b93
Austin Ricketts <austinricketts4@gmail.com>
2020-07-18 22:03:34 -0400

    styled About.js, still working on full mobile readiness

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,23 +8,29 @@ import CV from "./noun_Resume_10807.png";
 function Footer() {
     return (
       <footer className="footer">
-        <div className="container-fluid">
+        <div className="container">
           <div className = "row">
             <div className = "col-lg-6 offset-lg-3">
               <div className = "row">  
-                <div className = "col-lg-3 col-md-12 col-sm-12">
+                <div className = "">
                     <a href="https://www.linkedin.com/in/austin-ricketts-99034a110/" id="linkedin">
                       <img src={LinkedIn} alt="linkedin" target="_blank" className="footer-icon" /></a>
                   </div>
-                  <div className = "col-lg-3 col-md-12 col-sm-12">
+                  <br></br>
+                  <hr></hr>
+                  <div className = "">
                     <a href="mailto:austinricketts4@gmail.com" id="email">
                       <img src={GmailLogo} alt="gmail" target="_blank" className="footer-icon"/></a>
                   </div>
-                  <div className = "col-lg-3 col-md-12 col-sm-12">
+                  <br></br>
+                  <hr></hr>
+                  <div className = "">
                     <a href="https://github.com/Austin-Ricketts" id="github">
                       <img src={GithubLogo} alt="github" target="_blank" className="footer-icon"/></a>
                   </div>
-                  <div className = "col-lg-3 col-md-12 col-sm-12">
+                  <br></br>
+                  <hr></hr>
+                  <div className = "">
                     <a href="https://docs.google.com/document/d/1Ibsp4PCMnFLo1cSibFnJ5sqnkVvUrAkOlT0sZjtQcH4/edit?usp=sharing" id="github">
                       <img src={CV} alt="cv" target="_blank" className="footer-icon"/></a>
                   </div>                    
@@ -33,6 +39,32 @@ function Footer() {
           </div>
         </div>
       </footer>
+      // <footer className="footer">
+      //   <div className="container">
+      //     <div className = "row">
+      //       <div className = "col-lg-6 offset-lg-3">
+      //         <div className = "row">  
+      //           <div className = "col-lg-3 col-md-12 col-sm-12">
+      //               <a href="https://www.linkedin.com/in/austin-ricketts-99034a110/" id="linkedin">
+      //                 <img src={LinkedIn} alt="linkedin" target="_blank" className="footer-icon" /></a>
+      //             </div>
+      //             <div className = "col-lg-3 col-md-12 col-sm-12">
+      //               <a href="mailto:austinricketts4@gmail.com" id="email">
+      //                 <img src={GmailLogo} alt="gmail" target="_blank" className="footer-icon"/></a>
+      //             </div>
+      //             <div className = "col-lg-3 col-md-12 col-sm-12">
+      //               <a href="https://github.com/Austin-Ricketts" id="github">
+      //                 <img src={GithubLogo} alt="github" target="_blank" className="footer-icon"/></a>
+      //             </div>
+      //             <div className = "col-lg-3 col-md-12 col-sm-12">
+      //               <a href="https://docs.google.com/document/d/1Ibsp4PCMnFLo1cSibFnJ5sqnkVvUrAkOlT0sZjtQcH4/edit?usp=sharing" id="github">
+      //                 <img src={CV} alt="cv" target="_blank" className="footer-icon"/></a>
+      //             </div>                    
+      //         </div>
+      //       </div>
+      //     </div>
+      //   </div>
+      // </footer>
     );
   }
   
--- src/pages/About.js
+++ src/pages/About.js
@@ -7,33 +7,35 @@ function About() {
     <main>
         <div className="container">
             <div className="row" id="header">
-                <div className="col-4"></div>
-                <div className="col-4">
+                <div className="col-lg-5"></div>
+                <div className="col-lg-4">
                     <h1>Austin Ricketts</h1>
                 </div>
-                <div className="col-4"></div>
+                <div className="col-lg-2"></div>
             </div>
-            <div className="row" id="header">
-                <div className="col-4">
+            <div className="row"></div>
+            <br></br>
+            <div className="row" >
+                <div className="col-lg-4">
                     <img src={Austin} alt="austin" className="img-thumbnail"></img>
                 </div>
-                <div className="col-5"></div>
-                <div className="col-3"></div>
-            </div>
-            <div className="row" id="brand">
+                <div className="col-lg-4"></div>
+                <div className="col-lg-4" id="brand">
                 <p>
-                `I’m a front-end web developer, with skills on the back-end. 
-                I utilize information theory, visual art skills, and my pedagogical 
-                background to structure intuitive user experiences. I recently earned 
-                a certificate in full-stack development from the University of Pennsylvania, 
-                focusing on JavaScript, CSS, Node.js, and Object-Oriented programming. I develop 
-                responsive, mobile-first designs where form and function mingle toward marriage. 
-                Modernism with a soul is my motto. I’m motivated by highly-skilled teams, and 
-                pair programming, that harmonizes technical and aesthetic skills. And I recently 
-                worked on successful teams, within an agile framework, to create two well-received MVPs. 
-                I’m excited to continue learning, designing, and deploying with a quality-first team.`
-                </p>
+                    `I’m a front-end web developer, with skills on the back-end. 
+                    I utilize information theory, visual art skills, and my pedagogical 
+                    background to structure intuitive user experiences. I recently earned 
+                    a certificate in full-stack development from the University of Pennsylvania, 
+                    focusing on JavaScript, CSS, Node.js, and Object-Oriented programming. I develop 
+                    responsive, mobile-first designs where form and function mingle toward marriage. 
+                    Modernism with a soul is my motto. I’m motivated by highly-skilled teams, and 
+                    pair programming, that harmonizes technical and aesthetic skills. And I recently 
+                    worked on successful teams, within an agile framework, to create two well-received MVPs. 
+                    I’m excited to continue learning, designing, and deploying with a quality-first team.`
+                    </p>
+                </div>
             </div>
+            <div className="row" ></div>
         </div>
     </main>
     );
