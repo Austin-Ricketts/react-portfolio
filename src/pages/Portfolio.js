7d37871dbf4000171dc39d631c8d6c9d5676e0e1
Austin Ricketts <austinricketts4@gmail.com>
2020-07-22 15:31:34 -0400

    refactored code on Portfolio page, eliminated commented code, and spaced image elements

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -37,6 +37,7 @@ function Portfolio() {
                         repoLink={site[1].repoLink}
                     />
                 </div>
+                <br></br>
                 <div className="row">
                     <Projects
                         title={site[2].title}
@@ -52,6 +53,7 @@ function Portfolio() {
                         repoLink={site[3].repoLink}
                     />
                 </div>
+                <br></br>
                 <div className="row">
                     <Projects
                         title={site[4].title}
@@ -67,61 +69,6 @@ function Portfolio() {
                         repoLink={site[5].repoLink}
                     />
                 </div>
-                
-                
-                {/* <div className="row">
-                    <div className="col-4">
-                        <p class="titles">A Song in the Life</p>
-                        <a href="https://g0nzy.github.io/song-in-the-life/">
-                        <img src={Song} alt="Song in the Life"  class="img-thumbnail"></img>
-                        </a>
-                        <a href="https://github.com/G0nZy/song-in-the-life" className="titles">Repo Link</a>
-                    </div>
-                    <div className="col-4"></div>
-                    <div className="col-4">
-                        <p class="titles">Giph Your Times</p>
-                        <a href="https://ancient-depths-99799.herokuapp.com/?fbclid=IwAR3-aJRU1lUiURs_wBZ_gUJjfySUxFH8lkNONJkGM1R7Czi3X1KEhuwdXvs">
-                        <img src={Giph} alt="Giph Your Times" class="img-thumbnail"></img>
-                        </a>
-                        <a href="https://github.com/Austin-Ricketts/giph-you-times" className="titles">Repo Link</a>
-                    </div>
-                </div>
-                <br></br>
-                <div className="row">
-                    <div className="col-4">
-                        <p class="titles">Coding Quiz</p>
-                        <a href="https://austin-ricketts.github.io/Coding-Quiz/">
-                        <img src={Quiz} alt="Coding Quiz" class="img-thumbnail"></img>
-                        </a>
-                        <a href="https://github.com/Austin-Ricketts/Coding-Quiz" className="titles">Repo Link</a>
-                    </div>
-                    <div className="col-4"></div>
-                    <div className="col-4">
-                        <p class="titles">Austin's Meteorology</p>
-                        <a href="https://austin-ricketts.github.io/weather-app/">
-                        <img src={Weather} alt="Austin's Meteorology" class="img-thumbnail"></img>
-                        </a>
-                        <a href="https://github.com/Austin-Ricketts/weather-app" className="titles">Repo Link</a>
-                    </div>
-                </div>
-                <br></br>
-                <div className="row">
-                    <div className="col-4">
-                        <p class="titles">Work Day Scheduler</p>
-                        <a href="https://austin-ricketts.github.io/Work-Day-Scheduler/">
-                        <img src={Work} alt="Work Day Scheduler" class="img-thumbnail"></img>
-                        </a>
-                        <a href="https://github.com/Austin-Ricketts/Work-Day-Scheduler" className="titles">Repo Link</a>
-                    </div>
-                    <div className="col-4"></div>
-                    <div className="col-4">
-                        <p class="titles">README Generator</p>
-                        <a href="https://drive.google.com/file/d/1jGfjDUIr5avwcM6xryBwY20fVAwPlecH/view">
-                        <img src={Read} alt="README Generator" class="img-thumbnail"></img>
-                        </a>
-                        <a href="https://github.com/Austin-Ricketts/readme-generator" className="titles">Repo Link</a>
-                    </div>
-                </div> */}
             </div>
         </div>
     );
